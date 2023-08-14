af1ac82f61998762bfa70c1c256c6a4a7bc39ec2
Erfan <yaghoblo41@gmail.com>
2023-08-14 16:52:29 +0330

    add media config and change some styles

--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+import os
+from django.dispatch.dispatcher import receiver
 
 
 class Book(models.Model):
@@ -7,9 +9,24 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    book_cover = models.ImageField(upload_to='book-covers/', blank=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('book_detail', args=[self.id])
+
+    def delete(self, *args, **kwargs):
+        self.book_cover.delete()
+        super().delete(*args, **kwargs)
+
+
+@receiver(models.signals.pre_save, sender=Book)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    if instance.pk is not None:
+        old_instance = Book.objects.get(pk=instance.pk)
+        if old_instance.book_cover:    # چک کردن وجود عکس قبلی
+            if instance.book_cover != old_instance.book_cover:    # چک کردن تغییر در عکس
+                if os.path.isfile(old_instance.book_cover.path):   # چک کردن وجود فایل عکس قبلی
+                    os.remove(old_instance.book_cover.path)    # حذف فایل عکس قبلی
